b4010e434eae60e86f8139ba48992e427015a82b
unknown <SnowSneeze@protonmail.com>
2023-12-31 15:54:54 +0530

    init

---
+++ alloc.cc
@@ -0,0 +1,87 @@
+#include "basic.hh"
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#if(DBG)
+#include <stdio.h>
+#endif
+
+#define CHUNK_SIZE  (sizeof(u64)*2)
+#define CHUNK_COUNT 10000
+
+namespace allocator{
+    void *alloc(u64 size, char *memory, bool *stat){
+#if(DBG)
+	if(size == 0){
+	    printf("\n[MEM]: trying to allocate memory of size 0\n");
+	    return nullptr;
+	};
+#endif
+	/*
+	  Each allocation remembers the amount of blocks it asked for
+	  This decreases cache miss
+	*/
+	size += sizeof(u32);
+	u32 chunkReq = ceil(size/((double)(CHUNK_SIZE)));
+	u32 i = 0;
+    MEM_FIND_CHUNKS:
+	u32 startOff = i;
+	u32 chunkFound = 0;
+	while(stat[i] == false){
+	    chunkFound += 1;
+	    if(chunkFound == chunkReq){
+		char *ptr = memory+(startOff*CHUNK_SIZE);
+		u32 *intPtr = (u32*)ptr;
+		*intPtr = chunkReq;
+		ptr += sizeof(u32);
+		memset(&stat[startOff], true, sizeof(bool) * chunkFound);
+		return(void*)ptr;
+	    };
+	    i += 1;
+	};
+	if(i+chunkReq >= CHUNK_COUNT){
+#if(DBG)
+	    printf("\n[MEM]: out of chunks. Please increase CHUNK_COUNT\n");
+#endif
+	    return nullptr;
+	};
+	while(stat[i]){i += 1;};
+	goto MEM_FIND_CHUNKS;
+	return nullptr;
+    };
+    void free(void *ptr, char *memory, bool *stat){
+	char *cptr = (char*)ptr;
+	cptr -= sizeof(u32);
+	u32 off = (cptr - memory)/CHUNK_SIZE;
+	u32 chunks = *((u32*)cptr);
+	memset(&stat[off], false, sizeof(bool)*chunks);
+#if(DBG)
+	memset(cptr, 'A', CHUNK_SIZE*chunks);
+#endif
+    };
+};
+namespace mem{
+    char *memory;
+    bool *stat;
+    
+    void init(){
+	memory = (char*)malloc(CHUNK_SIZE   * CHUNK_COUNT);
+	const u64 statSize = sizeof(bool) * (CHUNK_COUNT + 1);   //NOTE: +1 for padding
+	stat   = (bool*)malloc(statSize);
+	memset(stat, false, statSize);
+	stat[CHUNK_COUNT] = true;
+#if(DBG)
+	memset(memory, 'A', CHUNK_SIZE * CHUNK_COUNT);
+#endif
+    };
+    void uninit(){
+	::free(memory);
+	::free(stat);
+    };
+    void* alloc(u64 size){
+	return allocator::alloc(size, memory, stat);
+    };
+    void free(void *ptr){
+	return allocator::free(ptr, memory, stat);
+    };
+};
